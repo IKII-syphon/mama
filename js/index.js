60b58a7a14ad2e2b04267d7cb350393b9372678c
IKII-syphon <rizkithunder6@gmail.com>
2024-09-22 16:12:53 +0700

    index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'untuk deza'.split('')
+const text = 'untuk mamak dari anak mu Rizki Syahputra'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
