0883731beae76680ef68f388d1d33fb36fe2c260
IKII-syphon <rizkithunder6@gmail.com>
2025-01-15 20:58:17 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
   
-      const titles = ('Terima kasih untuk segalanyaa!').split('')
+      const titles = ('Tetap semangat yaa, jangan sedih sedih mulu!').split('')
       const titleElement = document.getElementById('title');
       let index = 0;
   
